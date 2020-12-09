6b8d8b494098026c35c525f1bc3ee9689957206b
Ravi <ravipradhan@protonmail.com>
2020-12-09 23:01:57 +0000

    remove superfluous main code

--- api/main.go
+++ api/main.go
@@ -1,17 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-func main() {
-	http.HandleFunc("/", helloWorld)
-	log.Println("YO SERVER ON")
-	http.ListenAndServe(":8080", nil)
-}
-
-func helloWorld(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello world\n")
-}
+func main() {}
